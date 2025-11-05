aaa2a34f8ded7a49e66f01b5b60e6aac043c68cc
Imama-M <imama.meraj@ontariotechu.net>
2025-11-05 11:56:14 -0500

    Second Commit

--- routes/index.js
+++ routes/index.js
@@ -8,27 +8,22 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/home', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
 /* GET About page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Us' });
+router.get('/about', function(req, res,next) {
+  res.render('about', { title: 'About Me' });
 });
 
-/* GET products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products' });
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
 });
 
-/* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
-});
-
-/* GET home page. */
+/* GET Contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Us' });
+  res.render('contact', { title: 'Contact' });
 });
 module.exports = router;
